07794a5d7f58ce1575d7b50ed84d0cdb0dcb265e
Steven Linfield <S.Linfield@outlook.com>
2022-10-24 21:38:16 +0200

    Another commit

--- multiplexer-of-madness.py
+++ multiplexer-of-madness.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import numpy as np
+import scipy as sp
+from sp.scipy.stats import linregress
 
 path = 'C:/Users/SLinf/Documents/GitHub/multiplexer-of-madness/'
 file = 'test.csv'
 df = pd.read_csv(path + file, sep = ',', header = 5, encoding = "utf16")
-df.dropna(axis = 'rows')
-print(df)
+ndf = df.dropna()   # removes null values (blank in excel) from the dataframe
+
+array = ndf.to_numpy()
+print(array.shape)
 
