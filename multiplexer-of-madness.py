861b8be2dc4493ae2d7b9988234aaf4af1d358dd
Steven Linfield <S.Linfield@outlook.com>
2022-10-25 22:46:33 +0200

    Adding comments

--- multiplexer-of-madness.py
+++ multiplexer-of-madness.py
@@ -1,24 +1,31 @@
+'''MODULES'''
 import pandas as pd
 import numpy as np
 from sympy import diff
 from scipy.stats import linregress
-import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 
 '''Importing data to dataframe/array'''
-#test file already saved as Log10
+# Open file into a Pandas dataframe
 path = 'C:/Users/SLinf/Documents/GitHub/multiplexer-of-madness/'
 file = 'test.csv'
 df = pd.read_csv(path + file, sep = ',', header = 5, encoding = "utf16")
-ndf = df.dropna()   # removes null values (blank in excel) from the dataframe
+
+# Remove any null values from the dataframe
+ndf = df.dropna()
+
+# Convert the dataframe to an array
 array = ndf.to_numpy()
 
-
+print(array.shape)
 '''Changing array shape'''
+# Convert the 2D array given by excel into a 4D array: (channel, experiment, dataset, data)
+# Note: Managed to make a 4D array, but data was placed incorrectly during reshape
+# Still need to finish this, but for now, split the 2D array into 64 columns of 2D arrays and then reshaped
 split = np.split(array, int((array.shape[1])), axis = 1)
-resplitx = np.reshape(split[2], int(array.shape[0]))
-resplity = np.reshape(split[3], int(array.shape[0]))
+resplitx = np.reshape(split[14], int(array.shape[0]))
+resplity = np.reshape(split[15], int(array.shape[0]))
 
 
 '''Finding linear region'''
@@ -56,21 +63,6 @@ print(type(array))
 extraction = linregress(testx.astype(float), testy.astype(float))
 
 
-'''Data extraction'''
-
-with open('example.txt', 'w') as file:
-    for x in testy:
-        file.write(str(x) + '\n')
-
-#for x in narray[:,:,1,1]:
-    #print(x[:,2:4])
-    
-#find region of linear of each transistor
-#fit to linear region
-#plot data to confirm
-#use line of best fit to report
-#colour map
-
 '''Plotting data'''
 x = resplitx
 y = resplity
@@ -78,8 +70,18 @@ z = extraction[0]*testx + extraction[1]
 fig = plt.figure(num = 1, figsize = (6.4, 4.8), dpi = 100, facecolor = 'white', edgecolor = 'white', frameon = True)
 
 ax = fig.add_axes([0.2, 0.2, 0.7, 0.7])
-
+ax.set_title('R =' + str(extraction[2]), loc = 'center', pad = 20, fontsize = 15)
 ax.plot(x, y, linewidth = 1, linestyle = '-', color = 'green', marker = None, label = None)
-ax.plot(testx, z, linewidth = 2, linestyle = '-', color = 'blue', marker = None, label = 'R =' + str(extraction[2]))
+ax.plot(testx, z, linewidth = 2, linestyle = '-', color = 'blue', marker = None, label = None)
 plt.show()
 plt.close()
+
+
+'''Data extraction'''
+
+with open('example.txt', 'w') as file:
+    for x in testy:
+        file.write(str(x) + '\n')
+
+
+'''Colour map'''
