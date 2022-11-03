7f0c88a2612674d064e4bdafc2a9cc32663be2cb
Moonshine the P <68279135+MoonshinetheP@users.noreply.github.com>
2022-11-03 20:27:01 +0100

    Corrected input type for logarithm()

--- src/multiplexermanager/operations.py
+++ src/multiplexermanager/operations.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-def smooth():
+def smooth(input, smooth = 100):
+    np.convolve()
     pass
 
 def linear_regression():
@@ -10,17 +11,16 @@ def peakfinder():
     pass
 
 def logarithm(input):
-    working = input.array
-    output = np.empty((working.shape))
-    for x in range(0,working.shape[0]):
-        for y in range(0,working.shape[1]):
+    output = np.empty((input.shape))
+    for x in range(0,input.shape[0]):
+        for y in range(0,input.shape[1]):
             if input.bipot == True:
-                output[x,y,0,:] = working[x,y,0,:]
-                output[x,y,1,:] = np.log10(np.absolute(working[x,y,1,:]))
-                output[x,y,2,:] = working[x,y,2,:]
-                output[x,y,3,:] = np.log10(np.absolute(working[x,y,3,:]))
+                output[x,y,0,:] = input[x,y,0,:]
+                output[x,y,1,:] = np.log10(np.absolute(input[x,y,1,:]))
+                output[x,y,2,:] = input[x,y,2,:]
+                output[x,y,3,:] = np.log10(np.absolute(input[x,y,3,:]))
 
             else:
-                output[x,y,0,:] = working[x,y,0,:]
-                output[x,y,1,:] = np.log10(np.absolute(working[x,y,1,:]))
+                output[x,y,0,:] = input[x,y,0,:]
+                output[x,y,1,:] = np.log10(np.absolute(input[x,y,1,:]))
     return output
