36493f0a7acfcfa31b0a5d3a519b73c5366bf0b6
Moonshine the P <68279135+MoonshinetheP@users.noreply.github.com>
2022-11-04 10:30:56 +0100

    Added function descriptions and fixed smooth()

--- src/multiplexermanager/operations.py
+++ src/multiplexermanager/operations.py
@@ -1,13 +1,17 @@
 import numpy as np
 
 def smooth(input, window = 100):
+    '''Returns the input array with a moving average operation on each of the current values'''
     output = np.empty((input.shape))
     for ix,iy in np.ndindex((input.shape[0:2])):
+        output[ix,iy,0,:] = input[ix,iy,0,:]
         output[ix,iy,1,:] = np.convolve(input[ix,iy,1,:], window) / window
+        output[ix,iy,0,:] = input[ix,iy,2,:]
         output[ix,iy,3,:] = np.convolve(input[ix,iy,3,:], window) / window
     return output
 
 def peakfinder(input):
+    '''Returns the maximum and minimum current values/positions for each of the current values'''
     for ix,iy in np.ndindex((input.shape[0:2])):  
         imax = np.max(input[ix,iy,3,:])
         imax_index = np.argmax(input[ix,iy,3,:])
@@ -16,6 +20,7 @@ def peakfinder(input):
     return(imax,imax_index,imin,imin_index)
 
 def logarithm(input):
+    '''Returns the input array with a log10 operation on each of the current values'''
     output = np.empty((input.shape))
     for ix,iy in np.ndindex((input.shape[0:2])):
         output[ix,iy,0,:] = input[ix,iy,0,:]
